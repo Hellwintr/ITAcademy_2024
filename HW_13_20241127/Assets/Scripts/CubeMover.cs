d51ee5ed9eab3bf1d9b074c1bd6dd0d9a79abc00
DESKTOP-ED847MG\Captain <praetorian139@gmail.com>
2024-11-30 00:14:45 +0100

    no trim?

--- HW_13_20241127/Assets/Scripts/CubeMover.cs
+++ HW_13_20241127/Assets/Scripts/CubeMover.cs
@@ -4,6 +4,8 @@ public class CubeMover : MonoBehaviour
 {
     public float moveSpeed = 3.0f;
     private bool isMoving = false;
+    private static float lastCubePositionX;
+    private MeshFilter meshFilter;
     void Update()
     {
         if (isMoving)
@@ -31,5 +33,53 @@ public class CubeMover : MonoBehaviour
     private void StopMoving()
     {
         isMoving = false;
+        meshFilter = GetComponent<MeshFilter>();
+        meshFilter.mesh.MarkDynamic();
+        CheckAndTrim();
+    }
+    private void CheckAndTrim()
+    {
+        if (lastCubePositionX != 0)
+        {
+            float currentCubeLeftX = transform.position.x - GetWidth() / 2;
+            float currentCubeRightX = transform.position.x + GetWidth() / 2;
+            float lastCubeLeftX = lastCubePositionX - GetWidth() / 2;
+            float lastCubeRightX = lastCubePositionX + GetWidth() / 2;
+            if (currentCubeRightX > lastCubeRightX)
+            {
+                float trimWidth = currentCubeRightX - lastCubeRightX;
+                TrimCube(trimWidth, "right");
+            }
+
+            if (currentCubeLeftX < lastCubeLeftX)
+            {
+                float trimWidth = lastCubeLeftX - currentCubeLeftX;
+                TrimCube(trimWidth, "left");
+            }
+        }
+        lastCubePositionX = transform.position.x;
+    }
+
+    private void TrimCube(float trimWidth, string side)
+    {
+        Vector3[] vertices = meshFilter.mesh.vertices;
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            if (side == "right")
+            {
+                vertices[i] = new Vector3(transform.position.x + trimWidth / 2, vertices[i].y, vertices[i].z);
+            }
+            else if (side == "left")
+            {
+                vertices[i] = new Vector3(transform.position.x - trimWidth / 2, vertices[i].y, vertices[i].z);
+            }
+        }
+        meshFilter.mesh.vertices = vertices;
+        meshFilter.mesh.RecalculateNormals();
+    }
+
+    private float GetWidth()
+    {
+        return meshFilter.mesh.bounds.size.x;
     }
 }
